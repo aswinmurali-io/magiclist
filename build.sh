adea7d138072df96b88f8af0fedaa2cb3bd3dba7
aswinmurali-io <aswinmurali.co@gmail.com>
2020-09-02 19:44:01 +0530

    init setup

---
+++ build.sh
@@ -0,0 +1,1 @@
+cython magiclist.py -3
---
+++ magiclist.py
@@ -0,0 +1,17 @@
+import numpy
+
+__version__: str = "0.0.1"
+
+
+class MagicList(object):
+    """
+    Magic List main class containing the high level API implementation of the
+    next generation list with lot of features for modern problems
+    """
+    def __init__(self):
+        self._internal_list = []
+
+    def append(self, obj):
+        pass
+
+    
