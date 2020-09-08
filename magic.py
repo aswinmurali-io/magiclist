e44da46b1df44df8c5948205aa00ce7546932238
aswinmurali-io <aswinmurali.co@gmail.com>
2020-09-08 05:11:33 +0000

    added docstrings

--- magic.py
+++ magic.py
@@ -5,38 +5,82 @@ import threading
 
 class Magic(object):
     def __init__(self, name: str):
+        """Magic is the class that contains the magiclist datatype. Magiclist has the ability
+        to release memory on a per element basis rather than the object itself This way magiclist
+        can store a large about of data inside it without running out of memory. When the magiclist
+        runs out of memory, the purge() function can be called and all the inactive elements are
+        suspended from the memory and later on returned when needed. Magiclist can be used for
+        handling BIG data, perfect for data science.
+
+        Args:
+            name (str): The label used for accessing/creating the state of the magiclist.
+        """
         super().__init__()
         self.name: str = name
         self.memory: dict = {}
         self.loaded: bool = self.load()
 
-    def load(self):
+    def load(self) -> bool:
+        """The load() function can be used to load the previous data state from the storage device.
+
+        Returns:
+            bool: If loaded successfully or not
+        """
         if not os.path.exists(self.name):
             os.mkdir(self.name)
         items = glob.glob(f'{self.name}/*')
         if len(items) < 1:
             return False
-        for i in items:
-            self.get(i[len(self.name) + 1:])
-        return True
+        else:
+            for i in items:
+                self.get(i[len(self.name) + 1:])
+            return True
 
     def append(self, key: str, item: any) -> None:
+        """The append(...) function can be used to append a new element into the magic list.
+
+        Args:
+            key (str): The key used for accessing the data.
+            item (any): The content to store in the magic list with that above key as reference.
+        """
         self.memory[key] = [item, 0, 0]
         threading.Thread(target=lambda: open(f'{self.name}/{key}', 'w').write(
             f'{item} 0 0')).start()
 
     def sync(self, key: str, item: any, access_count: int,
              trend_ratio: int) -> None:
+        """The sync function can be used to sync the data changes given in the function args to the in-memory magic
+        list and to the storge-based (async) magiclist.
+
+        Args:
+            key (str): The key used for accessing the data.
+            item (any): The content to store in the magic list with that above key as reference.
+            access_count (int): The number of times the elements is been accessed.
+            trend_ratio (int): The access_count w.r.t. to a specific time interval.
+        """
         _fileptr = open(f'{self.name}/{key}', 'w')
         self.memory[key] = [item, access_count, trend_ratio]
         threading.Thread(target=lambda: _fileptr.write(
             f'{item} {access_count} {trend_ratio}')).start()
 
     def append_parallel(self, items: dict) -> None:
+        """The append_parallel(...) function can be used to batch append elements to the list.
+
+        Args:
+            items (dict): The dict of data to be added to the magiclist.
+        """
         for key in items:
             self.append(key, items[key])
 
     def get(self, key: str) -> any:
+        """The get(...) function is used to get an element from the magiclist via the key.
+
+        Args:
+            key (str): The key used for accessing the data.
+
+        Returns:
+            any: Get the element asked for via the key.
+        """
         try:
             self.memory[key][1] += 1
             self.sync(key, self.memory[key][0], self.memory[key][1],
@@ -51,7 +95,15 @@ class Magic(object):
                 pass
         return None
 
-    def gets(self, keys: list) -> list:
+    def gets(self, keys: iter) -> list:
+        """The gets(...) function is used to batch get elements w.r.t to all the keys specified
+
+        Args:
+            keys (iter): The keys used for accessing the datas.
+
+        Returns:
+            list: Get all the elements asked for via the keys.
+        """
         return [self.get(key) for key in keys]
 
     def __purge(self):
@@ -62,7 +114,9 @@ class Magic(object):
                 del self.memory[key]
 
     def purge(self):
-        threading.Thread(target=self.purge, args=(), daemon=True).start()
+        """The purge() function will release less used elements from memory reducing the memory size of the list.
+        """
+        threading.Thread(target=self.__purge, args=(), daemon=True).start()
 
 
 test: Magic = Magic("test")
@@ -74,4 +128,6 @@ if not test.loaded:
 test.get("2")
 print(test.memory)
 test.get("1")
+test.purge()
+
 print(test.memory)
